0a47b698be9a61d76594a8f733018daae74d865e
ref-humbold <rafkaleta@onet.pl>
2017-05-25 17:03:59 +0200

    Adnotacja DependencyConstructor

---
+++ DI-container/src/ref_humbold/di_container/DependencyConstructor.java
@@ -0,0 +1,12 @@
+package ref_humbold.di_container;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.CONSTRUCTOR)
+public @interface DependencyConstructor
+{
+}
