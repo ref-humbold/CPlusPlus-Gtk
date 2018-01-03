4d5b7b7e6f4ad11b19a3d311f368f44e34fed63a
ref-humbold <rafkaleta@onet.pl>
2018-01-03 20:59:08 +0100

    Wstęp do amplifiera.

---
+++ FPGA-SoundMixer/get_gain.v
@@ -0,0 +1,8 @@
+module get_gain(output reg[3:0] gain, input clk, input[1:0] change);
+	always @(posedge clk)
+	case(change)
+		2'b10: gain <= gain == 4'b0111 ? gain : gain + 1;
+		2'b01: gain <= gain == 4'b1000 ? gain : gain -1;
+		default: gain <= gain;
+	endcase
+endmodule
