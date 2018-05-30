abb0a62049a3675e265f1f9d9529cae96a5893e6
ref-humbold <rafkaleta@onet.pl>
2018-05-31 00:34:01 +0200

    Dokończenie intefejsu GTK.

--- PI-Generator/generator_pi.cpp
+++ PI-Generator/generator_pi.cpp
@@ -7,73 +7,106 @@
 class gtk_app
 {
 private:
-    Glib::RefPtr<Gtk::Application> application;
+    double pi_value;
     Glib::RefPtr<Gtk::Builder> builder;
+    Gtk::Window * main_window;
+    Gtk::Button * exit_button;
+    Gtk::Button * continue_button;
+    Gtk::ProgressBar * progress_bar;
+    Gtk::Label * label_1b;
+    Gtk::Label * label_2b;
 
 public:
-    gtk_app()
-        : application{Gtk::Application::create()},
-          builder{Gtk::Builder::create_from_file("generator.glade", "main_window")}
+    gtk_app() : builder{Gtk::Builder::create_from_file("generator.glade")}
     {
+        srand(time(nullptr));
+        this->get_components();
+        this->connect_signals();
     }
+
+    ~gtk_app()
+    {
+        delete this->exit_button;
+        delete this->continue_button;
+        delete this->progress_bar;
+        delete this->label_1b;
+        delete this->label_2b;
+    }
+
+    Gtk::Window & get_main_window()
+    {
+        return *(this->main_window);
+    }
+
+private:
+    void get_components();
+    void connect_signals();
+    void exit_button_clicked_cb();
+    void continue_button_clicked_cb();
+    double count_pi();
 };
 
-void exit_button_clicked_cb()
+void gtk_app::get_components()
 {
+    this->builder->get_widget("main_window", this->main_window);
+    this->builder->get_widget("exit_button", this->exit_button);
+    this->builder->get_widget("continue_button", this->continue_button);
+    this->builder->get_widget("progress_bar", this->progress_bar);
+    this->builder->get_widget("label_1b", this->label_1b);
+    this->builder->get_widget("label_2b", this->label_2b);
 }
 
-void continue_button_clicked_cb()
+void gtk_app::connect_signals()
 {
+    exit_button->signal_clicked().connect(sigc::mem_fun(*this, &gtk_app::exit_button_clicked_cb));
+    continue_button->signal_clicked().connect(
+        sigc::mem_fun(*this, &gtk_app::continue_button_clicked_cb));
 }
 
-double pi()
+void gtk_app::exit_button_clicked_cb()
+{
+    Gtk::Main::quit();
+}
+
+void gtk_app::continue_button_clicked_cb()
+{
+    progress_bar->set_fraction(0.0);
+    pi_value = count_pi();
+    label_1b->set_text(std::to_string(pi_value));
+    label_2b->set_text(std::to_string(pi_value - M_PI));
+}
+
+double gtk_app::count_pi()
 {
     long long int shot = 0;
     const long long int NUMBER = 1LL << 24;
+    const long long int STEP = NUMBER >> 5;
     const int SIZE = 32750;
 
-    for(long long int i = 0LL; i < NUMBER; ++i)
+    for(long long int j = 1LL; j <= NUMBER / STEP; ++j)
     {
-        double pos_x = (rand() % SIZE) / (1.0 * SIZE);
-        double pos_y = (rand() % SIZE) / (1.0 * SIZE);
-        double radius = sqrt(pos_x * pos_x + pos_y * pos_y);
-
-        if(i % 1000000 == 0)
+        for(long long int i = 0LL; i < STEP; ++i)
         {
-            for(int j = 0; j < i / 1000000; ++j)
-                printf("#");
+            double pos_x = (rand() % SIZE) / (1.0 * SIZE);
+            double pos_y = (rand() % SIZE) / (1.0 * SIZE);
+            double radius = sqrt(pos_x * pos_x + pos_y * pos_y);
 
-            printf("\n");
+            if(radius <= 1.0)
+                ++shot;
         }
 
-        if(radius <= 1.0)
-            ++shot;
+        progress_bar->set_fraction(j * STEP * 100.0 / NUMBER);
     }
-
-    printf("\n");
 
     return (4.0 * shot) / NUMBER;
 }
 
 int main()
 {
-    char read = ' ';
+    Glib::RefPtr<Gtk::Application> application = Gtk::Application::create("generator.liczby.pi");
+    gtk_app app_window;
 
-    printf("\t\tGENERATOR LICZBY PI\n");
-    srand(time(nullptr));
-
-    do
-    {
-        double pi_value = pi();
-
-        printf("    PI = %lf  (absolute error = %lf)\n\n", pi_value, pi_value - M_PI);
-
-        do
-        {
-            printf("  Write Q to exit, C to continue and press ENTER\n>>> ");
-            scanf(" %c", &read);
-        } while(read != 'q' && read != 'Q' && read != 'c' && read != 'C');
-    } while(read != 'q' && read != 'Q');
+    application->run(app_window.get_main_window());
 
     return 0;
 }
