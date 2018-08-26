728262a7c5213dcb029f97b3b20417dff8380229
R. Kaleta <rafkaleta@onet.pl>
2018-08-26 22:19:35 +0200

    SQL format

--- warsztat/init_db.sql
+++ warsztat/init_db.sql
@@ -1,26 +1,52 @@
 -- CREATE DOMAIN
-CREATE DOMAIN percentage AS integer NOT NULL DEFAULT 0 CHECK(VALUE IN(0, 5, 10, 15, 20, 25));
+CREATE DOMAIN percent_type AS integer NOT NULL DEFAULT 0 CHECK(VALUE IN(0, 5, 10, 15, 20, 25));
 
 CREATE DOMAIN auto_type AS char(1) NOT NULL DEFAULT 'o' CHECK(VALUE IN('o', 'c', 'a', 'm'));
 
 CREATE DOMAIN price_type AS numeric(10, 2) CHECK(VALUE > 0);
 
-CREATE DOMAIN registration AS text NOT NULL CHECK(VALUE ~ '(^[A-Z][A-Z] [A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]$)|(^[A-Z][A-Z][A-Z] [A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]?$)');
+CREATE DOMAIN registration_type AS text NOT NULL CHECK(VALUE ~ '(^[A-Z][A-Z] [A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]$)|(^[A-Z][A-Z][A-Z] [A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]?$)');
 
-CREATE DOMAIN phone AS integer CHECK(100000000 <= VALUE AND VALUE <= 999999999);
+CREATE DOMAIN phone_type AS integer CHECK(100000000 <= VALUE AND VALUE <= 999999999);
 
 -- CREATE TABLE
-CREATE TABLE clients (id serial PRIMARY KEY, imie text, nazwisko text, telefon phone, firma text DEFAULT NULL, rabat percentage);
+CREATE TABLE clients (id serial PRIMARY KEY,
+                     imie text,
+                     nazwisko text,
+                     telefon phone_type,
+                     firma text DEFAULT NULL,
+                     rabat percent_type);
 
-CREATE TABLE cars (model text PRIMARY KEY, marka text NOT NULL, typ auto_type, waga double precision CHECK(waga > 0));
+CREATE TABLE cars (model text PRIMARY KEY,
+                  marka text NOT NULL,
+                  typ auto_type,
+                  waga double precision CHECK(waga > 0));
 
-CREATE TABLE uslugi (nazwa text PRIMARY KEY, cena price_type);
+CREATE TABLE uslugi (nazwa text PRIMARY KEY,
+                    cena price_type);
 
-CREATE TABLE zlecenia (id serial PRIMARY KEY, data_zlec timestamp DEFAULT now(), data_real timestamp DEFAULT NULL, nr_rej registration, faktura boolean DEFAULT FALSE, koszt price_type, kli_id integer, sam_model text);
+CREATE TABLE zlecenia (id serial PRIMARY KEY,
+                      data_zlec timestamp DEFAULT now(),
+                      data_real timestamp DEFAULT NULL,
+                      nr_rej registration_type,
+                      faktura boolean DEFAULT FALSE,
+                      koszt price_type,
+                      kli_id integer,
+                      sam_model text);
 
-CREATE TABLE carparts (id serial PRIMARY KEY, producent text, ilosc integer CHECK(ilosc >= 0), typ text, uniw boolean DEFAULT TRUE);
+CREATE TABLE carparts (id serial PRIMARY KEY,
+                      producent text,
+                      ilosc integer CHECK(ilosc >= 0),
+                      typ text,
+                      uniw boolean DEFAULT TRUE);
 
-CREATE TABLE zamowienia (id serial PRIMARY KEY, data_zamow timestamp DEFAULT now(), data_real timestamp DEFAULT NULL, firma text, ilosc integer CHECK(ilosc > 0), cena price_type, cze_id integer);
+CREATE TABLE zamowienia (id serial PRIMARY KEY,
+                        data_zamow timestamp DEFAULT now(),
+                        data_real timestamp DEFAULT NULL,
+                        firma text,
+                        ilosc integer CHECK(ilosc > 0),
+                        cena price_type,
+                        cze_id integer);
 
 CREATE TABLE zleusl (zle_id integer, usl_nazwa text);
 
@@ -61,11 +87,24 @@ GRANT SELECT, INSERT, UPDATE ON zamowienia TO magazynier;
 GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO sprzedawca, magazynier, mechanik;
 
 -- CREATE OR REPLACE RULE / CREATE OR REPLACE FUNCTION / CREATE TRIGGER
-CREATE OR REPLACE RULE czesam_rule AS ON INSERT TO czesam DO ALSO UPDATE carparts SET uniw = FALSE WHERE carparts.id = NEW.cze_id;
+CREATE OR REPLACE RULE czesam_rule AS
+    ON INSERT TO czesam
+    DO ALSO UPDATE carparts
+            SET uniw = FALSE
+            WHERE carparts.id = NEW.cze_id;
 
 CREATE OR REPLACE FUNCTION zleusl_trg_func() RETURNS TRIGGER AS $f$
 BEGIN
-    WITH tab AS (SELECT zlecenia.id, sum(uslugi.cena) FROM zlecenia JOIN zleusl ON zleusl.zle_id = zlecenia.id JOIN uslugi ON zleusl.usl_nazwa = uslugi.nazwa GROUP BY zlecenia.id HAVING zlecenia.id = NEW.zle_id) UPDATE zlecenia SET koszt = tab.sum FROM tab WHERE zlecenia.id = tab.id;
+    WITH tab AS (SELECT zlecenia.id, sum(uslugi.cena)
+                FROM zlecenia
+                    JOIN zleusl ON zleusl.zle_id = zlecenia.id
+                    JOIN uslugi ON zleusl.usl_nazwa = uslugi.nazwa
+                GROUP BY zlecenia.id
+                HAVING zlecenia.id = NEW.zle_id)
+    UPDATE zlecenia SET koszt = tab.sum
+    FROM tab
+    WHERE zlecenia.id = tab.id;
+
     RETURN NULL;
 END;
 $f$ LANGUAGE plpgsql;
