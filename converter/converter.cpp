9824c1c3607a45ff2f032a3fb6909a68fb320bf3
R. Kaleta <rafkaleta@onet.pl>
2018-09-17 00:00:43 +0200

    Funkcja wydzialania nazwy katalogu

--- converter/converter.cpp
+++ converter/converter.cpp
@@ -246,15 +246,19 @@ void gtk_app::convert_button_clicked_cb()
 
 #pragma endregion
 
+std::string extract_directory(const char * full_path)
+{
+    std::string exec_path = std::string(full_path);
+    size_t dirpos = exec_path.find_last_of("/\\");
+
+    return exec_path.substr(0, dirpos);
+}
+
 int main(int argc, char * argv[])
 {
-    std::string exec_path = std::string(argv[0]);
-    size_t dirpos = exec_path.find_last_of("/\\");
-    std::string dir_path = exec_path.substr(0, dirpos);
-
     Glib::RefPtr<Gtk::Application> application =
         Gtk::Application::create(argc, argv, "converter.liczbowy");
-    gtk_app app_window(dir_path);
+    gtk_app app_window(extract_directory(argv[0]));
 
     application->run(app_window.get_main_window());
 
