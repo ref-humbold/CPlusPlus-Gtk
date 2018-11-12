ac0035ed409e210e124ae019c853742098fb40e1
R. Kaleta <rafkaleta@onet.pl>
2018-11-12 13:00:28 +0100

    Funkcja main

--- gomoku/src/gomoku.ml
+++ gomoku/src/gomoku.ml
@@ -1,6 +1,9 @@
-begin
-  Stat.prepare_data ();
-  Gui.new_window ();
-  Gui_runner.run_menu ();
-  Graphics.close_graph ();
-end;;
+let main () =
+  begin
+    Stat.prepare_data ();
+    Gui.new_window ();
+    Gui_runner.run_menu ();
+    Graphics.close_graph ();
+  end;;
+
+let _ = main ();;
