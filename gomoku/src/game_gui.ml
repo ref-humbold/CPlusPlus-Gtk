6397a1c370de1f645457dcd40969962306518476
R. Kaleta <rafkaleta@onet.pl>
2018-11-24 22:18:20 +0100

    Draw wider borders

--- gomoku/src/game_gui.ml
+++ gomoku/src/game_gui.ml
@@ -14,50 +14,50 @@ let get_borders size =
   and half = Gui.ratio 1 2 in
   (half + step * cols, half - step * cols);;
 
-let get_lines_pos size =
-  let cols = size / 2 + 1
+let get_lines size =
+  let cols = size / 2
   and half = Gui.ratio 1 2 in
-  let rec glp i acc =
+  let rec get_lines' i acc =
     if i + cols >= 0
-    then glp (i - 1) ((half + step * i)::acc)
+    then get_lines' (i - 1) @@ (half + step * i)::acc
     else acc in
-  glp cols [];;
+  get_lines' cols [];;
 
 let norm size (x, y) =
   let (_, endline) = get_borders size in
-  let nrm w = (w - endline + step / 2) / step in
-  (nrm x, nrm y);;
+  let norm' w = (w - endline + step / 2) / step in
+  (norm' x, norm' y);;
 
 let display size =
-  let pos = get_lines_pos size
+  let pos = get_lines size
   and (pbeg, pend) = get_borders size in
-  let rec draw_lines lst =
-    match lst with
-    | [] -> ()
-    | p::ps ->
-      begin
-        Graphics.set_color Graphics.black;
-        Graphics.moveto p pbeg;
-        Graphics.lineto p pend;
-        Graphics.moveto pbeg p;
-        Graphics.lineto pend p;
-        draw_lines ps
-      end in
+  let rec draw_line width pos =
+    begin
+      Graphics.set_line_width width;
+      Graphics.set_color Graphics.black;
+      Graphics.moveto pos pbeg;
+      Graphics.lineto pos pend;
+      Graphics.moveto pbeg pos;
+      Graphics.lineto pend pos
+    end in
   begin
     Gui.clear_window Graphics.cyan;
-    draw_lines pos
+    draw_line 3 pbeg;
+    draw_line 3 pend;
+    List.iter (draw_line 0) pos
   end;;
 
-let draw_stone size ply (row, col) =
+let draw_stone size player (row, col) =
   let (_, endline) = get_borders size in
-  let stone_color =
-    match ply with
+  let stone_colour =
+    match player with
     | Board.Human -> Graphics.white
     | Board.Comp -> Graphics.black
     | Board.Blocked -> raise @@ Board.Incorrect_player "Game_gui.draw_stone" in
-  let (px, py) = (endline + col * step, endline + row * step) in
+  let px = endline + col * step
+  and py = endline + row * step in
   begin
-    Graphics.set_color stone_color;
+    Graphics.set_color stone_colour;
     Graphics.fill_circle px py (7 * step / 16)
   end;;
 
