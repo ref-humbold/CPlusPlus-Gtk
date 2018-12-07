2bea1b41ed0cd5e7c4f09289ed8d7302fc9b3805
R. Kaleta <rafkaleta@onet.pl>
2018-12-07 19:12:13 +0100

    Removed extracted Gomoku and Interpreter-APO

--- gomoku/src/gomoku.ml
+++
@@ -1,9 +0,0 @@
-let main () =
-  begin
-    Stat.prepare_data ();
-    Gui.new_window ();
-    Gui_runner.run_menu ();
-    Graphics.close_graph ();
-  end;;
-
-let _ = main ();;
--- gomoku/src/human_player.ml
+++
@@ -1,5 +0,0 @@
-let rec move size gameboard =
-  let pos = Game_gui.choose_stone size in
-  if Board.is_free pos gameboard
-  then pos
-  else move size gameboard;;
