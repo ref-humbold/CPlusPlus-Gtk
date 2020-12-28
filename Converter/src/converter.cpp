0e8fff482c56d70849887ec7ca2c59305969c427
R. Kaleta <rafkaleta@onet.pl>
2020-12-28 23:16:54 +0100

    Simple tests

--- Converter/src/converter.cpp
+++ Converter/src/converter.cpp
@@ -2,7 +2,7 @@
 
 using namespace std::string_literals;
 
-converter::converter(int base_in, int base_out) : base_in{base_in}, base_out{base_out}
+converter::converter(size_t base_in, size_t base_out) : base_in{base_in}, base_out{base_out}
 {
     if(base_in < 2 || base_in > 16)
         throw converter_exception("Invalid input base ("s + std::to_string(base_in)
@@ -15,9 +15,9 @@ converter::converter(int base_in, int base_out) : base_in{base_in}, base_out{bas
 
 std::string converter::convert(const std::string & number) const
 {
-    sign sgn = get_sign(number);
+    sign sign_ = get_sign(number);
 
-    validate_digits(sgn, number);
+    validate_digits(sign_, number);
 
     if(base_in == base_out)
         return number;
@@ -25,7 +25,7 @@ std::string converter::convert(const std::string & number) const
     long long int decimal = to_decimal(number);
     std::vector<int> number_out = to_base_out(decimal);
 
-    return build_number(sgn, number_out);
+    return build_number(sign_, number_out);
 }
 
 converter::sign converter::get_sign(const std::string & number) const
@@ -43,24 +43,22 @@ converter::sign converter::get_sign(const std::string & number) const
     }
 }
 
-void converter::validate_digits(converter::sign sgn, const std::string & number) const
+void converter::validate_digits(converter::sign sign_, const std::string & number) const
 {
-    size_t digits_begin = sgn != nosign ? 1 : 0;
-    char max_dec = static_cast<char>(std::min(57LL, base_in + 47LL));
-    char max_big_hex = static_cast<char>(std::max(64LL, base_in + 54LL));
-    char max_small_hex = static_cast<char>(std::max(96LL, base_in + 86LL));
+    size_t digits_begin = sign_ != nosign ? 1 : 0;
+    char max_dec = static_cast<char>(std::min((size_t)'9', '0' + base_in - 1));
+    char max_big_hex = static_cast<char>(std::max((size_t)'A' - 1, 'A' + base_in - 11));
+    char max_small_hex = static_cast<char>(std::max((size_t)'a' - 1, 'a' + base_in - 11));
 
     if(number.length() == digits_begin)
         throw converter_exception("No number specified"s);
 
     for(auto it = number.begin() + digits_begin; it != number.end(); ++it)
-    {
         if(*it < '0' || (*it > max_dec && *it < 'A') || (*it > max_big_hex && *it < 'a')
            || *it > max_small_hex)
             throw converter_exception("Character \'"s + std::string(1, *it)
                                       + "\' is invalid for numeral system of base "s
                                       + std::to_string(base_in));
-    }
 }
 
 long long int converter::to_decimal(const std::string & number) const
@@ -101,11 +99,11 @@ std::vector<int> converter::to_base_out(long long int decimal) const
     return output;
 }
 
-std::string converter::build_number(converter::sign sgn, const std::vector<int> & number) const
+std::string converter::build_number(converter::sign sign_, const std::vector<int> & number) const
 {
     std::string result;
 
-    if(sgn == minus)
+    if(sign_ == minus)
         result.push_back('-');
 
     for(auto it = number.rbegin(); it != number.rend(); ++it)
--- Converter/test/converter_test.cpp
+++ Converter/test/converter_test.cpp
@@ -1,2 +1,22 @@
 #include <gtest/gtest.h>
 #include "converter.hpp"
+
+TEST(ConverterTest, convert_WhenPositiveInput_ThenPositiveResult)
+{
+    // given
+    converter test_object(10, 2);
+    // when
+    std::string result = test_object.convert("10");
+    //then
+    EXPECT_EQ("1010", result);
+}
+
+TEST(ConverterTest, convert_WhenNegativeInput_ThenNegativeResult)
+{
+    // given
+    converter test_object(10, 2);
+    // when
+    std::string result = test_object.convert("-10");
+    //then
+    EXPECT_EQ("-1010", result);
+}
