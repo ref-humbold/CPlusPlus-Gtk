ad8c005b1ddce611f216fb2a554405acbe3bc770
R. Kaleta <rafkaleta@onet.pl>
2021-01-07 21:25:51 +0100

    More converter tests

--- Converter/test/converter_test.cpp
+++ Converter/test/converter_test.cpp
@@ -6,9 +6,9 @@ TEST(ConverterTest, convert_WhenPositiveInput_ThenPositiveResult)
     // given
     converter test_object(10, 2);
     // when
-    std::string result = test_object.convert("10");
+    std::string result = test_object.convert("+25");
     //then
-    EXPECT_EQ("1010", result);
+    EXPECT_EQ("11001", result);
 }
 
 TEST(ConverterTest, convert_WhenNegativeInput_ThenNegativeResult)
@@ -16,7 +16,63 @@ TEST(ConverterTest, convert_WhenNegativeInput_ThenNegativeResult)
     // given
     converter test_object(10, 2);
     // when
-    std::string result = test_object.convert("-10");
+    std::string result = test_object.convert("-25");
     //then
-    EXPECT_EQ("-1010", result);
+    EXPECT_EQ("-11001", result);
+}
+
+TEST(ConverterTest, convert_WhenLeadingZeros_ThenResultWithoutLeadingZeros)
+{
+    // given
+    converter test_object(8, 10);
+    // when
+    std::string result = test_object.convert("000157");
+    //then
+    EXPECT_EQ("111", result);
+}
+
+TEST(ConverterTest, convert_WhenInputBaseOver10_ThenLettersAreCaseIndependent)
+{
+    // given
+    converter test_object(16, 10);
+    // when
+    std::string result = test_object.convert("c3F");
+    //then
+    EXPECT_EQ("3135", result);
+}
+
+TEST(ConverterTest, convert_WhenOutputBaseOver10_ThenUppercaseLettersAsDigits)
+{
+    // given
+    converter test_object(10, 16);
+    // when
+    std::string result = test_object.convert("1200");
+    //then
+    EXPECT_EQ("4B0", result);
+}
+
+TEST(ConverterTest, convert_WhenInvalidInputBase_ThenConverterException)
+{
+    // when
+    auto exec = []() { return converter(1, 10); };
+    //then
+    EXPECT_THROW(exec(), converter_exception);
+}
+
+TEST(ConverterTest, convert_WhenInvalidOutputBase_ThenConverterException)
+{
+    // when
+    auto exec = []() { return converter(10, 25); };
+    //then
+    EXPECT_THROW(exec(), converter_exception);
+}
+
+TEST(ConverterTest, convert_WhenInvalidDigits_ThenConverterException)
+{
+    // given
+    converter test_object(4, 10);
+    // when
+    auto exec = [&]() { return test_object.convert("109"); };
+    //then
+    EXPECT_THROW(exec(), converter_exception);
 }
